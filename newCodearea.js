852cf1a4b7519ce07898f7c1832bb6b336a18d29
Dadsdy <94847478+theRealDadsdy@users.noreply.github.com>
2023-05-15 13:40:36 -0400

    Update newCodearea.js

--- newCodearea.js
+++ newCodearea.js
@@ -19,17 +19,44 @@ function newCode(parent = document.body) {
     }
   });
   textarea.addEventListener('keyup', event => {
-    const numberOfLines = event.target.value.split('\n').length
+    const numberOfLines = event.target.value.split('\r').join('\n').split('\n').length
     lineNumbers.innerHTML = Array(numberOfLines)
       .fill('<span></span>')
       .join('')
+    if (event.key == 'Tab') {
+      event.preventDefault();
+    }
+    if (event.key = 'Enter'){
+      event.preventDefault();
+    }
   });
   textarea.addEventListener('keydown', event => {
     if (event.key === 'Tab') {
-      const start = textarea.selectionStart
-      const end = textarea.selectionEnd
-      textarea.value = textarea.value.substring(0, start) + '\t' + textarea.value.substring(end)
-      event.preventDefault()
+      const start = textarea.selectionStart;
+      const end = textarea.selectionEnd;
+      document.execCommand('insertText', false, '  ');
+      event.preventDefault();
+    }
+    if (event.key == 'Enter'){
+      const start = textarea.selectionStart;
+      const end = textarea.selectionEnd;
+      const prev = textarea.value.substring(0, start).split('\n');
+      const line = prev[prev.length - 1];
+      const brace = line.split('{').length - line.split('}').length;
+      const unmatched = (textarea.value.split("{").length > textarea.value.split("}").length);
+      let newLine = '\n' + line.split('  ').fill('').join('  ');
+      let loc = start + line.split('  ').length * 2 - 1;
+      if (brace > 0 && unmatched){
+        newLine += '  ' + newLine + '}';
+        loc += 2;
+      }
+      if (brace > 0 && !unmatched){
+        newLine += '  ';
+        loc += 2;
+      }
+      document.execCommand('insertText', false, newLine);
+      textarea.setSelectionRange(loc, loc);
+      event.preventDefault();
     }
   });
   return div;
